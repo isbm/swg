782fc982cd12d846509140471592f3eff89731f7
Bo Maryniuk <bo@suse.de>
2016-04-02 14:25:43 +0200

    Add an example test app

---
+++ test.py
@@ -0,0 +1,21 @@
+from places import Places
+from places import place
+
+
+places = Places("db/places")
+for x in range(10):
+    address = place.Address('Erlangen', 'Scheissstrasse', x)
+    contact = place.Contact('123-123-123', 'bla@google.com', 'foobar', 'N/A')
+    info = place.Info(700, rooms=2, sqm=50, day=16, month=5, memo='some free text here, e.g. your mama is fat!')
+    apartment = place.Apartment(address=address, contact=contact, info=info)
+    places.add_apartment(apartment)
+places.commit()
+
+print "-" * 80
+print "Found apartments:"
+print "-" * 80
+for apartment in places.list_apartments():
+    print "Address:", apartment.address
+    print "Contact:", apartment.contact
+    print "Info:", apartment.info
+    print "." * 80
